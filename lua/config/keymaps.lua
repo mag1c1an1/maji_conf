c965e86b6b461be12745b20bf81f29fd397fce5b
mag1c1an1 <mag1cian@icloud.com>
2023-06-05 17:32:52 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---local Util = require("lazyvim.util")
+local Util = require("lazyvim.util")
 
--- local function map(mode, lhs, rhs, opts)
---   local keys = require("lazy.core.handler").handlers.keys
---   ---@cast keys LazyKeysHandler
---   -- do not create the keymap if a lazy keys handler exists
---   if not keys.active[keys.parse({ lhs, mode = mode }).id] then
---     opts = opts or {}
---     opts.silent = opts.silent ~= false
---     vim.keymap.set(mode, lhs, rhs, opts)
---   end
--- end
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
---map("t", "<c-\\>", "<cmd>ToggleTerm direction=float<cr>")
---map("n", "<c-\\>", "<cmd>ToggleTerm direction=float<cr>")
+local lazyterm = function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end
+
+map("t", "<c-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
