0671db1f1e76bcef35a77058931aad61c5a3ca38
mag1c1an1 <mag1cian@icloud.com>
2023-07-11 01:42:46 +0800

    update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,9 @@ return {
   cmd = "Copilot",
   build = ":Copilot auth",
   opts = {
-    suggestion = { enabled = true, auto_trigger = true },
+    suggestion = { enabled = true, auto_trigger = true, keymap = {
+      accept = "<C-l>",
+    } },
     panel = { enabled = true },
     filetypes = {
       markdown = true,
