a6ecc39e07f90c3a9a9afee24160125e267e962b
mag1c1an1 <mag1cian@icloud.com>
2025-07-18 01:29:41 +0800

    add cursor autocmd
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Terminal Mappings
 vim.keymap.set("n", "<c-\\>", function() Snacks.terminal() end, { desc = "Terminal (Terminal (cwd))" })
-vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+vim.keymap.set("n", "<c-\\>", function() Snacks.terminal() end, { desc = "Terminal (Terminal (cwd))" })
+vim.keymap.set("i", "jk", "<esc>", {silent = true})
