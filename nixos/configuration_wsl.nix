3b357d4203c6e954068d0760b50a34806c41d8c0
mag1cian <mag1cian@icloud.com>
2025-10-14 16:29:19 +0000

    add common to wsl
    Signed-off-by: mag1cian <mag1cian@icloud.com>
    Change-Id: I6a6a6964ad01abd30a1a77e7d3bc3c8ec3f710e6

--- nixos/configuration_wsl.nix
+++ nixos/configuration_wsl.nix
@@ -1,16 +1,25 @@
 let
   isWsl = builtins.getEnv "WSL" == "TRUE";
 in
-  {nixpkgs, ...}: {
+  {
+    pkgs,
+    nixpkgs,
+    lib,
+    ...
+  }: {
+    imports = [
+      ./common.nix
+    ];
     system.stateVersion = "25.05";
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     wsl = {
       enable = isWsl;
       defaultUser = "mag1cian";
       extraBin = [
-        {src = "${nixpkgs.coreutils}/bin/uname";}
-        {src = "${nixpkgs.coreutils}/bin/mkdir";}
-        {src = "${nixpkgs.coreutils}/bin/cp";}
-        {src = "${nixpkgs.git}/bin/git";}
+        {src = "${pkgs.coreutils}/bin/uname";}
+        {src = "${pkgs.coreutils}/bin/mkdir";}
+        {src = "${pkgs.coreutils}/bin/cp";}
+        {src = "${pkgs.git}/bin/git";}
       ];
     };
   }
