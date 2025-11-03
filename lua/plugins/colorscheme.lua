7ba19c75a6ebe0da778a5d20d4614e288876cf54
mag1c1an1 <mag1cian@icloud.com>
2025-11-03 10:44:32 +0800

    add onedark
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 -- Configure LazyVim to load gruvbox
 return {
+  { "olimorris/onedarkpro.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      -- colorscheme = "tokyonight-night",
+      colorscheme = "onelight",
     },
   },
 }
