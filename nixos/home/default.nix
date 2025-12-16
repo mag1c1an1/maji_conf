e727b564bb461cddc6f728cb407b5499c3ba3f96
mag1cian <mag1cian@icloud.com>
2025-12-16 13:23:34 +0000

    change java prio

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -79,7 +79,7 @@
     pkgs.scala_2_12
 
     # java
-    # pkgs.temurin-jre-bin-11
+    (pkgs.lowPrio pkgs.temurin-jre-bin-11)
     pkgs.gradle
     pkgs.maven
     pkgs.jdt-language-server # java
@@ -95,10 +95,10 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "25.05";
-  programs.java = {
-    enable = true;
-    package = pkgs.temurin-jre-bin-11;
-  };
+  # programs.java = {
+  #   enable = false;
+  #   package = pkgs.temurin-jre-bin-11;
+  # };
   home.file = {
 	  ".cargo/config.toml" = {
 		text = ''
