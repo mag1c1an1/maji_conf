6e63cad94cbbe33a1eb537225de67c467f7f9821
mag1cian <mag1cian@icloud.com>
2026-01-27 14:19:22 +0800

    wezterm use tokyo-night storm
    Signed-off-by: mag1cian <mag1cian@icloud.com>
    Change-Id: I8c39c3560b3649721a98a2b291e4ada16a6a6964

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,6 @@ local config = wezterm.config_builder()
 config.default_cursor_style = "BlinkingUnderline"
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 20
-config.color_scheme = 'Mariana'
 config.use_fancy_tab_bar = true
 config.show_new_tab_button_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
@@ -22,6 +21,9 @@ config.window_padding = {
   top = 5,
   bottom = 2,
 }
+
+-- config.color_scheme = 'Mariana'
+config.color_scheme = 'Tokyo Night Storm'
 
 -- Finally, return the configuration to wezterm:
 return config
