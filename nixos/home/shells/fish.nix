2e344ab6f5af2c42dd211f13b1e6144404b97c2d
mag1cian <mag1cian@icloud.com>
2026-05-09 19:51:53 +0000

    update fish
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- nixos/home/shells/fish.nix
+++ nixos/home/shells/fish.nix
@@ -16,8 +16,12 @@
       cnpm = "npm --registry=https://registry.npmmirror.com  --cache=$HOME/.npm/.cache/cnpm  --disturl=https://npmmirror.com/mirrors/node  --userconfig=$HOME/.cnpmrc";
     };
     interactiveShellInit = ''
-      if command -q nu
-        exec nu
+      if status is-interactive
+          and not set -q SSH_ORIGINAL_COMMAND
+          and test -t 0
+          and command -q nu
+
+          exec nu
       end
     '';
   };
