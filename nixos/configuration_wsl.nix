88028da83315f27520ad9e76e0e8fc8db68ad1dd
mag1cian <mag1cian@icloud.com>
2026-05-11 15:12:46 +0000

    add openssh
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- nixos/configuration_wsl.nix
+++ nixos/configuration_wsl.nix
@@ -12,6 +12,22 @@ in
     ];
     system.stateVersion = "25.05";
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    services.openssh = {
+      enable = true;
+      ports = [2222];
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+        X11Forwarding = false;
+        AllowUsers = [ "mag1cian" ];
+      };
+    };
+    users.users.mag1cian = {
+      openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBUNrW/gxDVgH5M2YH5B6edo/0T4c3G85SmlZUMKwVGFCQaKvJz8uL0KjBjSAVNLzxDUV36DNhr82p31jcaDE4WdeglNKbTzcVfD1uNyNhD1yypiYNocFasSGaqOGQkxhcfq/NrUqeZjojWxXQDE9fjAeHRCodLcoE/DczIltc2ya7dIxWb9RUSzrSgQSOS0d2ZrQtruYfCSd00UKZlSogO3NKKgFCl+3ElwUirHK2icJ1yRtcEeKaW3W1vSVIn0Y+nR0KJmfAP1gEh8+VJkwQeuXcE6YAP2QLWCo6FKbEYevKMTNvrsWGx6BNQHVyTvZh67AjMZP4zQ3Tg77funu9"
+    ];
+    };
     wsl = {
       enable = isWsl;
       defaultUser = "mag1cian";
