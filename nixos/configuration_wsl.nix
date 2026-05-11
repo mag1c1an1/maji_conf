44a8f3c426b6f8146aee84ba459868b4f0d6cbf1
mag1cian <mag1cian@icloud.com>
2026-05-11 16:16:56 +0000

    add frpc
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- nixos/configuration_wsl.nix
+++ nixos/configuration_wsl.nix
@@ -23,6 +23,27 @@ in
         AllowUsers = [ "mag1cian" ];
       };
     };
+    services.frp = {
+      enable = true;
+      role = "client";
+      settings = {
+        serverAddr = "101.132.60.231";
+        serverPort = 7000;
+        auth = {
+          method = "token";
+          token = "wsl-nixos";
+        };
+        proxies = [
+          {
+            name = "nixos-ssh";
+            type = "tcp";
+            localIP = "127.0.0.1";
+            localPort = 2222;
+            remotePort = 6000;
+          }
+        ];
+      };
+    };
     users.users.mag1cian = {
       openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBUNrW/gxDVgH5M2YH5B6edo/0T4c3G85SmlZUMKwVGFCQaKvJz8uL0KjBjSAVNLzxDUV36DNhr82p31jcaDE4WdeglNKbTzcVfD1uNyNhD1yypiYNocFasSGaqOGQkxhcfq/NrUqeZjojWxXQDE9fjAeHRCodLcoE/DczIltc2ya7dIxWb9RUSzrSgQSOS0d2ZrQtruYfCSd00UKZlSogO3NKKgFCl+3ElwUirHK2icJ1yRtcEeKaW3W1vSVIn0Y+nR0KJmfAP1gEh8+VJkwQeuXcE6YAP2QLWCo6FKbEYevKMTNvrsWGx6BNQHVyTvZh67AjMZP4zQ3Tg77funu9"
