a2b20ad0c63de5c08c5b9e045a760a91582d0c45
mag1cian <mag1cian@icloud.com>
2026-05-12 03:23:35 +0000

    nvim osc52
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- nixos/configuration_wsl.nix
+++ nixos/configuration_wsl.nix
@@ -48,14 +48,14 @@ in
       enable = true;
       settingsFile = "/home/mag1cian/maji_conf/xray/client.json";
     };
-    systemd.services.frpc-hw = {
-      description = "FRP HW";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart = "${pkgs.frp}/bin/frpc -c /etc/frp/hw.toml";
-        Restart = "always";
-      };
-    };
+    # systemd.services.frpc-hw = {
+    #   description = "FRP HW";
+    #   wantedBy = [ "multi-user.target" ];
+    #   serviceConfig = {
+    #     ExecStart = "${pkgs.frp}/bin/frpc -c /etc/frp/hw.toml";
+    #     Restart = "always";
+    #   };
+    # };
   users.users.mag1cian = {
     openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBUNrW/gxDVgH5M2YH5B6edo/0T4c3G85SmlZUMKwVGFCQaKvJz8uL0KjBjSAVNLzxDUV36DNhr82p31jcaDE4WdeglNKbTzcVfD1uNyNhD1yypiYNocFasSGaqOGQkxhcfq/NrUqeZjojWxXQDE9fjAeHRCodLcoE/DczIltc2ya7dIxWb9RUSzrSgQSOS0d2ZrQtruYfCSd00UKZlSogO3NKKgFCl+3ElwUirHK2icJ1yRtcEeKaW3W1vSVIn0Y+nR0KJmfAP1gEh8+VJkwQeuXcE6YAP2QLWCo6FKbEYevKMTNvrsWGx6BNQHVyTvZh67AjMZP4zQ3Tg77funu9"
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,18 +5,19 @@ local uname = vim.uv.os_uname().release:lower()
 
 if uname:find("microsoft") or uname:find("wsl") then
   -- WSL
-  vim.g.clipboard = {
-    name = "win32yank",
-    copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
-  }
+  vim.g.clipboard = "osc52"
+  -- vim.g.clipboard = {
+  --   name = "win32yank",
+  --   copy = {
+  --     ["+"] = "win32yank.exe -i --crlf",
+  --     ["*"] = "win32yank.exe -i --crlf",
+  --   },
+  --   paste = {
+  --     ["+"] = "win32yank.exe -o --lf",
+  --     ["*"] = "win32yank.exe -o --lf",
+  --   },
+  --   cache_enabled = 0,
+  -- }
 
 elseif uname:find("orbstack") then
   -- macOS OrbStack
