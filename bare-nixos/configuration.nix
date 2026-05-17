089c0cf8eaca8b57d390c31d7639cbe50dc4722f
mag1c1an1 <mag1cian@icloud.com>
2026-05-17 19:07:37 +0800

    update
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- bare-nixos/configuration.nix
+++ bare-nixos/configuration.nix
@@ -1,14 +1,17 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ inputs, config, lib, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -26,11 +29,21 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = true;  # see the note above
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.open = true; # see the note above
   hardware.nvidia.modesetting.enable = true;
 
+  services.logind.settings.Login = {
+    IdleAction = "ignore";
+  };
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 
+  nix.optimise.automatic = true;
   # networking.hostName = "AUROSX"; # Define your hostname.
 
   # Configure network connections interactively with nmcli or nmtui.
@@ -52,24 +65,22 @@
   # };
 
   fonts = {
-      packages = with pkgs; [
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        noto-fonts-color-emoji
+    packages = with pkgs; [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
 
-        nerd-fonts.hack
-        nerd-fonts._0xproto
-      ];
-    };
+      nerd-fonts.hack
+      nerd-fonts._0xproto
+    ];
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -90,8 +101,8 @@
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   # 启用 Flakes 特性以及配套的船新 nix 命令行工具
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # 启用 Flakes 特性以及配套的船新 nix 命令行工具
+  nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.substituters = [
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     "https://cache.nixos.org"
@@ -101,16 +112,18 @@
 
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-    # Flakes 通过 git 命令拉取其依赖项，所以必须先安装好 git
-    git
-    fish
-    neovim
-    just
-    pkg-config
-  ] ++ [
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      # Flakes 通过 git 命令拉取其依赖项，所以必须先安装好 git
+      git
+      fish
+      neovim
+      just
+      pkg-config
+    ]
+    ++ [
+      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
   programs.fish.enable = true;
   programs.git.enable = true;
   programs.nix-ld.enable = true;
@@ -118,17 +131,47 @@
   environment.variables.EDITOR = "nvim";
 
   services.xray = {
-      enable = true;
-      settingsFile = "/home/maji/maji_conf/xray/client.json";
-	};
+    enable = true;
+    settingsFile = "/home/maji/maji_conf/xray/client.json";
+  };
 
-  services.keyd = {
-    enable = false;
-    # keyboards.default.settings = {
-    #   main = {
-    #     capslock = "overload(f15, capslock)";
-    #   };
-    # };
+  services.openssh = {
+    enable = true;
+    ports = [2222];
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      X11Forwarding = false;
+      AllowUsers = ["mag1cian"];
+    };
+  };
+
+  services.frp = {
+    enable = true;
+    role = "client";
+    settings = {
+      serverAddr = "101.132.60.231";
+      serverPort = 7000;
+      auth = {
+        method = "token";
+        token = "wsl-nixos";
+      };
+      proxies = [
+        {
+          name = "nixos-ssh";
+          type = "tcp";
+          localIP = "127.0.0.1";
+          localPort = 2222;
+          remotePort = 6000;
+        }
+      ];
+    };
+  };
+  users.users.maji = {
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBUNrW/gxDVgH5M2YH5B6edo/0T4c3G85SmlZUMKwVGFCQaKvJz8uL0KjBjSAVNLzxDUV36DNhr82p31jcaDE4WdeglNKbTzcVfD1uNyNhD1yypiYNocFasSGaqOGQkxhcfq/NrUqeZjojWxXQDE9fjAeHRCodLcoE/DczIltc2ya7dIxWb9RUSzrSgQSOS0d2ZrQtruYfCSd00UKZlSogO3NKKgFCl+3ElwUirHK2icJ1yRtcEeKaW3W1vSVIn0Y+nR0KJmfAP1gEh8+VJkwQeuXcE6YAP2QLWCo6FKbEYevKMTNvrsWGx6BNQHVyTvZh67AjMZP4zQ3Tg77funu9"
+    ];
   };
   services.kanata = {
     enable = true;
@@ -152,7 +195,7 @@
     };
   };
 
-   # timezone and local
+  # timezone and local
   time.timeZone = "Asia/Shanghai";
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -163,7 +206,6 @@
     LC_PAPER = "zh_CN.UTF-8";
     LC_CTYPE = "zh_CN.UTF-8";
   };
-
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -190,13 +232,13 @@
   # system.copySystemConfiguration = true;
   security.sudo.wheelNeedsPassword = false; # sudo组是否需要密码
   users.users.maji = {
-     shell = pkgs.fish;
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       google-chrome
-       github-cli
-     ];
+    shell = pkgs.fish;
+    isNormalUser = true;
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      google-chrome
+      github-cli
+    ];
   };
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
@@ -216,5 +258,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
