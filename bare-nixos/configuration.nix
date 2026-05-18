bfd9b58c458de4c0c2cc1c20f390fb646b045854
mag1c1an1 <mag1cian@icloud.com>
2026-05-19 01:09:08 +0800

    update
    Signed-off-by: mag1c1an1 <mag1cian@icloud.com>

--- bare-nixos/configuration.nix
+++ bare-nixos/configuration.nix
@@ -37,16 +37,41 @@
   i18n.inputMethod = {
     type = "fcitx5";
     enable = true;
+    fcitx5 = {
+      addons = with pkgs; [
+        (fcitx5-rime.override {
+          rimeDataPkgs = [pkgs.rime-ice];
+        })
+        qt6Packages.fcitx5-configtool
+        qt6Packages.fcitx5-chinese-addons # 拼音、五笔等
+      ];
+      waylandFrontend = true;
+      settings = {
+        globalOptions = {
+          # "Behavior" = {
+          # 关键设置：关闭程序内预编辑，强制使用 Fcitx5 浮窗(能解决 neovide 中无法预编辑的问题)
+          # "PreeditEnabledByDefault" = "False";
+          # };
+          "Hotkey/TriggerKeys" = {"0" = "F10";};
+        };
+        inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "rime";
+        };
+      };
+    };
   };
-  # services.displayManager.dms-greeter = {
-  #     enable = true;
-  #     compositor.name = "niri";
-  #     package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.default;
-  #   };
 
   services.logind.settings.Login = {
     IdleAction = "ignore";
   };
+
   boot.loader.systemd-boot.configurationLimit = 10;
   nix.gc = {
     automatic = true;
--- bare-nixos/home/fcitx5/default.nix
+++ bare-nixos/home/fcitx5/default.nix
@@ -1,38 +1,4 @@
 {pkgs, ...}: {
-
-  # 添加常用中文模块
-  i18n.inputMethod.fcitx5.addons = with pkgs; [
-    (fcitx5-rime.override {
-      rimeDataPkgs = [pkgs.rime-ice];
-    })
-    qt6Packages.fcitx5-configtool
-    qt6Packages.fcitx5-chinese-addons # 拼音、五笔等
-  ];
-  i18n.inputMethod.fcitx5.waylandFrontend = true;
-
-  # 设置rime输入法与美式键盘英文输入法并排序rime第一位
-  i18n.inputMethod.fcitx5.settings.inputMethod = {
-    GroupOrder."0" = "Default";
-    "Groups/0" = {
-      Name = "Default";
-      "Default Layout" = "us";
-      DefaultIM = "keyboard-us";
-    };
-    "Groups/0/Items/0".Name = "keyboard-us";
-    "Groups/0/Items/1".Name = "rime";
-  };
-
-  i18n.inputMethod.fcitx5.settings = {
-    globalOptions = {
-      # "Behavior" = {
-      # 关键设置：关闭程序内预编辑，强制使用 Fcitx5 浮窗(能解决 neovide 中无法预编辑的问题)
-      # "PreeditEnabledByDefault" = "False";
-      # };
-      "Hotkey/TriggerKeys" = {"0" = "F10";};
-    };
-    # addons.classicui.globalSection.Theme = "catppuccin-mocha-mauve";
-  };
-
   home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ../../rime/default.custom.yaml;
 
   # catppuccin的flake,option
