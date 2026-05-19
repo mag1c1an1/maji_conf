30a0fb71a7c45cf2daa5cdc8581a3c6ed07c1429
mag1c1an1 <mag1cian@icloud.com>
2026-05-19 10:29:19 +0800

    update

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,19 +5,18 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.default_cursor_style = "BlinkingBar"
+-- font
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 20
-config.use_fancy_tab_bar = true
-config.show_new_tab_button_in_tab_bar = false
+-- tab
 config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
 config.window_decorations = "RESIZE" -- disable title
-
-config.initial_cols = 80
-config.initial_rows = 100
-
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-
+-- window size
+config.initial_cols = 80
+config.initial_rows = 100
 config.window_padding = {
   left = 5,
   right = 5,
@@ -27,6 +26,9 @@ config.window_padding = {
 
 -- config.color_scheme = 'Mariana'
 config.color_scheme = 'Tokyo Night Storm'
+-- render
+config.animation_fps = 60
+config.max_fps = 60
 
 local act = wezterm.action
 
