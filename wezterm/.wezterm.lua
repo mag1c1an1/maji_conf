903ee85a9968fde67f3e9cbc570d2a8f227812f1
mag1c1an1 <mag1cian@icloud.com>
2026-05-19 10:26:12 +0800

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.default_cursor_style = "BlinkingUnderline"
+config.default_cursor_style = "BlinkingBar"
 config.font = wezterm.font 'Hack Nerd Font'
 config.font_size = 20
 config.use_fancy_tab_bar = true
@@ -15,6 +15,9 @@ config.window_decorations = "RESIZE" -- disable title
 config.initial_cols = 80
 config.initial_rows = 100
 
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
 config.window_padding = {
   left = 5,
   right = 5,
@@ -24,6 +27,15 @@ config.window_padding = {
 
 -- config.color_scheme = 'Mariana'
 config.color_scheme = 'Tokyo Night Storm'
+
+local act = wezterm.action
+
+config.keys = {
+  { key = '{', mods = 'SHIFT|ALT', action = act.MoveTabRelative(-1) },
+  { key = '}', mods = 'SHIFT|ALT', action = act.MoveTabRelative(1) },
+}
+
+
 
 -- Finally, return the configuration to wezterm:
 return config
