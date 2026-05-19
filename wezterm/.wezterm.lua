16653311b545c255345d1f974a7d95e733db46cb
mag1c1an1 <mag1cian@icloud.com>
2026-05-19 11:13:11 +0800

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -25,7 +25,11 @@ config.window_padding = {
 }
 
 -- config.color_scheme = 'Mariana'
-config.color_scheme = 'Tokyo Night Storm'
+
+local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').dawn
+config.colors = theme.colors()
+
+-- config.color_scheme = 'Tokyo Night Storm'
 -- render
 config.animation_fps = 60
 config.max_fps = 60
