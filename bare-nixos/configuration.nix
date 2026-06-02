0a7ba6b6f5f7e651e46cc974282a86091c1bde75
mag1cian <mag1cian@icloud.com>
2026-06-02 16:09:35 +0800

    update
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- bare-nixos/configuration.nix
+++ bare-nixos/configuration.nix
@@ -178,6 +178,13 @@
     "/share/xdg-desktop-portal"
   ];
 
+  systemd.services.nix-daemon.environment = {
+    HTTP_PROXY = "http://127.0.0.1:10801";
+    HTTPS_PROXY = "http://127.0.0.1:10801";
+    ALL_PROXY = "socks5://127.0.0.1:10800";
+    NO_PROXY = "127.0.0.1,localhost,::1";
+  };
+
   # programs.firefox.enable = true;
   # programs.dms-shell = {
   #   enable = true;
