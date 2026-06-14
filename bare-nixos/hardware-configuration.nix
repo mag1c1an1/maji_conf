baa8b30fb4064644b5ce1c6169aabf54420cde7f
mag1cian <mag1cian@icloud.com>
2026-06-14 20:31:39 +0800

    update
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- bare-nixos/hardware-configuration.nix
+++ bare-nixos/hardware-configuration.nix
@@ -22,10 +22,10 @@
     fsType = "btrfs";
   };
 
-  fileSystems."/home/maji/data" = {
-    device = "/dev/disk/by-uuid/66040a38-67a6-420d-8b14-b65b253dfe3d";
-    fsType = "btrfs";
-  };
+  # fileSystems."/home/maji/data" = {
+  #   device = "/dev/disk/by-uuid/66040a38-67a6-420d-8b14-b65b253dfe3d";
+  #   fsType = "btrfs";
+  # };
 
   fileSystems."/efi" = {
     device = "/dev/disk/by-uuid/DC47-E186";
