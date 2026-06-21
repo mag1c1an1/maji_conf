fdd5251f579fc17c192137e9135c00dbaca340a5
mag1cian <mag1cian@icloud.com>
2026-06-21 12:36:19 +0800

    update
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- bare-nixos/home/shells/fish.nix
+++ bare-nixos/home/shells/fish.nix
@@ -25,7 +25,8 @@
       fish_add_path $HOME/.npm-global/bin
 
       # cargo
-      set -gx CARGO_HOME $HOME/data/.cargo
+      # set -gx CARGO_HOME $HOME/data/.cargo
+      set -gx CARGO_HOME $HOME/.cargo
       fish_add_path $CARGO_HOME/bin
 
 
