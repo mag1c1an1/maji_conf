258eb58e428b629f289268e6baf36cfb14a49770
mag1cian <mag1cian@icloud.com>
2026-07-08 13:28:44 +0800

    update
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- bare-nixos/configuration.nix
+++ bare-nixos/configuration.nix
@@ -179,10 +179,10 @@
   ];
 
   systemd.services.nix-daemon.environment = {
-    HTTP_PROXY = "socks5://127.0.0.1:10800";
-    HTTPS_PROXY = "socks5://127.0.0.1:10800";
-    ALL_PROXY = "socks5://127.0.0.1:10800";
-    NO_PROXY = "127.0.0.1,localhost,::1";
+    # HTTP_PROXY = "socks5://127.0.0.1:10800";
+    # HTTPS_PROXY = "socks5://127.0.0.1:10800";
+    # ALL_PROXY = "socks5://127.0.0.1:10800";
+    # NO_PROXY = "127.0.0.1,localhost,::1";
   };
 
   # programs.firefox.enable = true;
