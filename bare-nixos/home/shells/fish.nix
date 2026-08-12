5b321a6c6faac699ac77285da835c33c33ad83e2
mag1cian <mag1cian@icloud.com>
2026-08-12 14:39:03 +0800

    update
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- bare-nixos/home/shells/fish.nix
+++ bare-nixos/home/shells/fish.nix
@@ -53,7 +53,7 @@
         else
           set -gx HTTP_PROXY http://127.0.0.1:7890
           set -gx HTTPS_PROXY http://127.0.0.1:7890
-          set -gx ALL_PROXY socks5://127.0.0.1:7890
+          set -gx ALL_PROXY http://127.0.0.1:7890
         end
       end
 
