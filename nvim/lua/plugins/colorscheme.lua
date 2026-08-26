4045c37b0e26dd3406f65dcd72fbe0c318d910e3
mag1cian <mag1cian@icloud.com>
2026-08-26 12:55:52 +0800

    update
    Signed-off-by: mag1cian <mag1cian@icloud.com>

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,19 @@
 -- Configure LazyVim to load gruvbox
 return {
   { "olimorris/onedarkpro.nvim", priority = 1000 },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "latte", -- 浅色: latte,深色: mocha / macchiato / frappe
+    },
+  },
   {"rose-pine/neovim", name = "rose-pine", lazy=true,},
   {
     "LazyVim/LazyVim",
     opts = {
-        colorscheme = "tokyonight-night",
-      -- colorscheme = "rose-pine-dawn",
+        colorscheme = "catppuccin",
     },
   },
 }
